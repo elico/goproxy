2914ecaaf626215c83ef377e208dbf5524629d1e
elico <eliezer@ngtech.co.il>
2016-05-07 06:27:51 +0300

    Trying to fix an issue with responses that are missing content type

--- responses.go
+++ responses.go
@@ -19,7 +19,9 @@ func NewResponse(r *http.Request, contentType string, status int, body string) *
 	resp.Request = r
 	resp.TransferEncoding = r.TransferEncoding
 	resp.Header = make(http.Header)
-	resp.Header.Add("Content-Type", contentType)
+	if contentType != "" {
+		resp.Header.Add("Content-Type", contentType)
+	}
 	resp.StatusCode = status
 	buf := bytes.NewBufferString(body)
 	resp.ContentLength = int64(buf.Len())
@@ -34,5 +36,5 @@ const (
 
 // Alias for NewResponse(r,ContentTypeText,http.StatusAccepted,text)
 func TextResponse(r *http.Request, text string) *http.Response {
-	return NewResponse(r, ContentTypeText, http.StatusAccepted, text)
+	return NewResponse(r, """, http.StatusAccepted, text)
 }
